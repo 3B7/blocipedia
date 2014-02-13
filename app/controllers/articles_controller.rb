88dc5a75294074e77d48dfa4c8b4b97d84a00a12
erik <erikbatres@gmail.com>
2014-02-13 07:32:30 +0000

    Added create and update CRUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,38 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      flash[:notice] = "Article was saved."
+      redirect_to @article
+    else
+      flash[:error] = "There was an eror saving the article. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      flash[:notice] = "Post was updated."
+      redirect_to @article
+    else
+      flash[:error] = "There was an error updating the post. Please try again."
+      render :edit
+    end
+  end
+
+
 end
