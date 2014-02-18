82d3d9d9651403d920fdb34bf122c46f885df1d5
vagrant <vagrant@rails-dev-box>
2014-02-18 06:57:29 +0000

    updating wiki model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'faker'
 
+wikis = []
+15.times do
+  wikis << Article.create(
+    title: Faker::Lorem.words(rand(1..10)).join(" "), 
+    body: Faker::Lorem.paragraph(rand(1..4))
+  )
+end
+
+
 rand(4..10).times do
   password = Faker::Lorem.characters(10)
   u = User.new(
@@ -11,17 +20,43 @@ rand(4..10).times do
   u.save
 
 rand(5..12).times do
+    wiki = wikis.first
     p = u.articles.create(
+      wiki: wiki,
       title: Faker::Lorem.words(rand(1..10)).join(" "), 
       body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
     # set the created_at to a time within the past year
     p.update_attribute(:created_at, Time.now - rand(600..31536000))
+
+    wikis.rotate!
   end
 end
 
-u = User.first
-u.skip_reconfirmation!
-u.update_attributes(email: 'john@example.com', password: 'helloworld', password_confirmation: 'helloworld')
+u = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'admin')
+
+u = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
+u.update_attribute(:role, 'moderator')
+
+u = User.new(
+  name: 'Member User',
+  email: 'member@example.com', 
+  password: 'helloworld', 
+  password_confirmation: 'helloworld')
+u.skip_confirmation!
+u.save
 
 puts "Seed finished"
 puts "#{Wiki.count} wikis created"
